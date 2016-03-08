1d4dccfc91a119b99c7bd5e6d4e8efaa83d9cdfc
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2016-03-08 16:46:42 +1000

    start building Crontab::Schedule

--- lib/crontab/schedule.rb
+++ lib/crontab/schedule.rb
@@ -2,13 +2,32 @@
 # vim: ts=2 sts=2 sw=2 expandtab
 
 require_relative 'entry'
+require_relative 'env'
 
 module Crontab
   class Schedule
-    # TODO:
-    #   * list of ENV settings/vars
-    #   * list of Entry objects
-    #   * Schedule.from(str) / Schedule.from_file(fh) / ?
+
+    def initialize
+      @env = Crontab::Env.new
+      @entries = []
+    end
+
+    # @param [File|IO] input -- something that responds to #each_line
+    def interpret input
+      input.each_line.each_with_index do |line, lineno|
+        line.sub! /#.*$/, ''
+        line.strip!
+        if !line.empty?
+          if @env.recognise? line
+            @env.parse(line, lineno)
+          else
+            @entries << Crontab::Entry.from(line, lineno)
+          end
+        end
+      end
+      # TODO: rationalise the entries
+    end
+
   end
 end
 
